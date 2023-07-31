774811478866929dbceaec49e2d60df3f8f78380
SiarheiZhachek <SiarheiZhachek@yandex.ru>
2023-07-31 15:51:34 +0300

    my_test

--- sun_num.py
+++
@@ -1,8 +0,0 @@
-num = int(input())
-
-
-def sum_num():
-    num_sum = num + 5
-    print(num_sum)
-
-sum_num()
--- test.py
+++
@@ -1,8 +0,0 @@
-def one():
-    a_num = 5
-    b_num = 2
-    c_num = a_num + b_num
-    print(c_num)
-
-
-one()
